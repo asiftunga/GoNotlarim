abdc6991f37424baa42b0c9a3497ed9171068bad
asif tunga mubarek <asiftunga@hotmail.com>
2022-06-30 10:33:26 +0300

    modul kullanmayi ogrendim

---
+++ internal/invoice/invoice.go
@@ -0,0 +1,8 @@
+package invoice
+
+import "fmt"
+
+// create the invoice for the reservation
+func CreateAndSaveInvoice(name string, nights uint, price float32) {
+    fmt.Println("invoice paketinden geldi")
+}
