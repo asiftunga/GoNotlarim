d712189c2ad663e342eddc985ea81553966c5511
asif tunga mubarek <asiftunga@hotmail.com>
2022-08-17 10:18:02 +0300

    ceyrek kisim tekrar edildi ve gozden gecirildi

--- cmd/first.go
+++ cmd/first.go
@@ -598,16 +598,17 @@ func main(){
     err := newcart.Lock()
 }
 ! receiverli metotlari gormek icin internal/cart/cart.go kismina git
-bu kodun soyle bir mantigi var aslinda. internal klasoru icersinde cart isimli bir klasorum var o klasorun icerisinde cart.go isimli bir paketim (source code'a paket ismi veriliyordu) var. Bu paketi import kisminda import etmisim. Daha sonra bu paketimin icerisinde Cart isimli bir struct'im ve iki adet metodum var. Metodlarimin receiveleri Cart tipinde oldugundan dolayi cart tipinde bir sey olmadan kullanamam bundan dolayi metotlarimi kullanabilmek icin newcart isimli Cart tipindeki cart.Cart{} olusturdum, daha sonra bunu kullanarak metotlarima erisebildim
-=====================================peki value receiver mi yoksa pointer receiver mi kullanmaliyim?=======================
+bu kodun soyle bir mantigi var aslinda. internal klasoru icersinde cart isimli bir klasorum var o klasorun icerisinde cart.go isimli bir paketim (source code'a paket ismi veriliyordu) var. Bu paketi ustteki kodda import kisminda import etmisim. Daha sonra bu paketimin icerisinde Cart isimli bir struct'im ve iki adet metodum var. Metodlarimin receiveleri Cart tipinde oldugundan dolayi cart tipinde bir degisken olmadan metodlarimi kullanamam, bundan dolayi metotlarimi kullanabilmek icin newcart isimli Cart tipindeki cart.Cart{} olusturdum, daha sonra bunu kullanarak metotlarima erisebildim
+=====================================//@ peki value receiver mi yoksa pointer receiver mi kullanmaliyim?=======================
 eger value receiver olusturursam gonderdigim degisken method icerisinde kopyasi (internal) olusturulur ve o sekilde kullanilir  yani gonderdigim degisken degistirilmez.
 ! ayrica ustteki kullanim cok az da olsa memory kisminda fazlalik olusturur. Bu cogu zaman goz ardi edilebilir ancak memory onemli olan kullanimlarda dikkat edilmelidir
 ancak eger pointer tipinde bir degisken yollarsam yolladigim degiskenin adresi gideceginden dolayi yollayacagim degisken degistirilir
-@receiver kullanirken genel kullanim type in first letteridir. c *Cart gibi
+@ receiver kullanirken genel kullanim type in first letteridir. c *Cart gibi
 */
 
 // methods/example-project/main.go
-/*package main
+
+/* package main
 
 import (
 	"fmt"
@@ -618,24 +619,23 @@ import (
 
 func main() {
 
-    var newcart = cart.Cart{}
-    total,error := newcart.TotalPrice()
+    var newCart = cart.Cart{}   //(same as newcart:=cart.Cart{})
+    total,error := newCart.TotalPrice()
     fmt.Printf("toplam tutar : %d hata kodu : %d",total,error)
 
-	/*
+	totalPrice, err := newCart.TotalPrice()
+	if err != nil {
+	    fmt.Printf("impossible to compute price of the cart: %s", err)
+	    return
+	}
+	fmt.Println("Total Price", totalPrice.Display())
 
-	   totalPrice, err := newCart.TotalPrice()
-	   if err != nil {
-	       fmt.Printf("impossible to compute price of the cart: %s", err)
-	       return
-	   }
-	   fmt.Println("Total Price", totalPrice.Display())
-
-	   err = newCart.Lock()
-	   if err != nil {
-	       fmt.Printf("impossible to lock the cart: %s", err)
-	       return
-	   } */
+	err = newCart.Lock()
+	if err != nil {
+	    fmt.Printf("impossible to lock the cart: %s", err)
+	    return
+	}
+} */
 
 //?================================================================================================================
 //? bu kod blogu sadece receiver fonksiyonlari ve struct yapisini daha iyi anlamak icin var
@@ -647,12 +647,14 @@ import (
 	"fmt"
 	"newgroupproject/internal/receiver"
     _"strconv"
-)
+) */
 
+//alttaki koddaki receiver ismi yukarida import ettigim newgroupproject/internal/receiver isminden geliyor. (10 saat bunu dusundum amk)
+/* 
 func main(){
     //bunu pointer seklinde kullanmadigimda da oluyor peki pointerin buradaki amaci nedir amk
     //? burdaki b yi su sekilde kullandigimda neden bir fark olusmuyor bunu sonradan tekrar incelemem gerekli cok ONEMLI (b := receiver.Fatura)
-    var b *receiver.Fatura = &receiver.Fatura{
+    var b *receiver.Fatura = &receiver.Fatura{ 
         Isim: "Market Faturasi",
         Bahsis: 40.00,
         Items: map[string]float32{"patates": 5.00, "elma":4.00, "biber":10.00, "salatalik":12.00},
@@ -664,7 +666,7 @@ func main(){
     b.Update("lahana",17.00)
     fmt.Println(b.Hesap())
 }
-*/
+ */
 
 //?===========================================================================================================
 //leetcode sorusu : roman to int
@@ -768,22 +770,44 @@ ilk olarak upper fonksiyonu eucontries sliceinin bir local kopyasini olusturdu
 bu fonksiyonun icerisinde slice elementinin valuelarini degistirdik
 bu kopya hala temel olan slice i referans ediyor ancak soyle bir durum var //! sadece ama sadece var olan degerleri degistririsek bu olur demek istedigim slice icerisinde var olan elemanlar degistirilebilir bunun nedeni ise:
 @ aklimda daha kolay kalmasi icin sanki icerisindeki her bir value orijinal olan slice in valuelarina referans gosteriyormuslar gibi dusunebilirim
-//! ancak yeni eleman eklersem bu sonucumuza yansimaz.
+//! ancak yeni eleman eklersem bu sonucumuza yansimaz. Pointer kullanmadan orijinal degere yeni eleman eklenmesi yapilamaz
 */
 
-/* func main() {
-    EUcountries := []string{"Austria", "Belgium", "Bulgaria"}
-    addCountries2(&EUcountries) //slice adresini yolladik
-    fmt.Println(EUcountries)
-}
+// package main
 
-func addCountries2(countriesPointer *[]string) { //@ parametre olarak slice pointeri veriyoruz
-    *countriesPointer = append(*countriesPointer, []string{"Croatia", "Republic of Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"}...)
-} */
+// import "fmt"
+
+
+// func main() {
+//     EUcountries := []string{"Austria", "Belgium", "Bulgaria"}
+//     addCountries2(&EUcountries) //slice adresini yolladik
+//     fmt.Println(EUcountries)
+// }
+// func addCountries2(countriesPointer *[]string) { //@ parametre olarak slice pointeri veriyoruz
+//     *countriesPointer = append(*countriesPointer, []string{"Croatia", "Republic of Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"}...)
+// }
 
 // kisaca addcountries2 fonksiyonuna bir adres bilgisi yollandi ve fonksiyon kendi icerisinde olusturdugu degisken ile bu adres bilgisinin degerine  * operatoru ile ulasti
 
-//simdi bir kullanim gosterecegim. dereference operatorunu kullanmadan sadece adres bilgisi ile struct kullanabiliriz
+//? ============================================= receiver fonksiyon kullanarak bu sekilde yapilir ===============================================
+/* type EUcountries []string
+
+func main() {
+    hey := EUcountries{"austuria,belgium,danmark"}
+    hey.addCountries2()
+    fmt.Println(hey)
+} */
+
+// func (countriesPointer *EUcountries) addCountries2() { //@  receiver olarak slice pointeri veriyoruz
+//     *countriesPointer = append(*countriesPointer, []string{"Croatia", "Republic of Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"}...)
+// }
+//? ===============================================================================================================================================
+
+
+
+
+//simdi bir kullanim gosterecegim. adres bilgisi ile struct kullanabiliriz
+
 /* func main(){
 
     type Cart struct {
